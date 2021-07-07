a38170f392d2a88f5afe194cf8818c6b823a43f6
sandip123 <sandipkurhade.1988@gmail.com>
2021-07-07 19:42:31 +0530

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,14 @@ const request =require('request');
 const forecast=(latitude,longitude,callback)=>{
     const url='http://api.weatherstack.com/current?access_key=454c0049fe069369be298498a2aba7da&query=' + longitude+',' + latitude+'&units=f'
     request({url,json:true},(error,{body})=>{
-        const {weather_descriptions,temperature,feelslike} =body.current;
+        const {weather_descriptions,temperature,feelslike,humidity} =body.current;
         if(error){
 callback('Unable to connect to weather stack..!!',undefined)
         }else if(error){
             callback('Unable To fide location.',undefined);
         }
         else{
-            callback(undefined,weather_descriptions[0] +". It is currently " + temperature + " Fahrenheit out. It's feels like  " + feelslike + " Fahrenheit out.")
+            callback(undefined,weather_descriptions[0] +". It is currently " + temperature + " Fahrenheit out. It's feels like  " + feelslike + " Fahrenheit out. And currently humidity is " +humidity + ' %');
         }
     });
 };
